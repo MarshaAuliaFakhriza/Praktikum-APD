5a71fbe70f983e4cf8323796a5966df7a1454899
MarshaAuliaFakhriza <ghonkai850@gmail.com>
2024-10-16 16:22:48 +0800

    Add files via upload
    2409106116 Marsha Aulia Fakhriza C2'24 code pertemuan 7

---
+++ Praktikum-APD/pertemuan 7/pertemuan_7.py
@@ -0,0 +1,31 @@
+# def luas_persegi_panjang(panjang, lebar):
+#     luas = panjang * lebar
+#     print ("luas persegi panjang:", luas)
+
+# def luas_persegi(sisi):
+#     luas = sisi * sisi
+#     return luas
+
+# hasil = luas_persegi(4) + luas_persegi(2)
+# print(hasil)
+
+# def nilai(angka):
+#     if angka >10:
+#         print("1")
+#         #return
+#     else:
+#         print("2")
+
+data = 10
+
+def nilai():
+    data = 20
+    print(data)
+
+nilai()
+print(data)
+
+def mhs(nama1, nama2, nama3):
+    print(nama2)
+
+mhs(nama3 = "ucup", nama2 = "saipul", nama1 = "michael")
